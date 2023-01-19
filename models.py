299feead7c0c929e4ac1e8b2971ae945fa95ccc9
Sebastian Ament <sebastianament@meta.com>
2023-01-19 10:26:09 -0800

    added models, utilities, and january proposal

---
+++ models.py
@@ -0,0 +1,223 @@
+from typing import Any, List, Optional, Tuple, Union
+
+import torch
+from botorch import fit_gpytorch_model
+from botorch.models import FixedNoiseGP, ModelList, ModelListGP, SingleTaskGP
+from botorch.models.gpytorch import BatchedMultiOutputGPyTorchModel
+from botorch.models.model import Model
+from botorch.models.transforms.input import (
+    AffineInputTransform,
+    ChainedInputTransform,
+    Log10,
+    Normalize,
+)
+from botorch.models.transforms.outcome import Standardize
+from gpytorch import ExactMarginalLogLikelihood
+from gpytorch.constraints import GreaterThan
+from gpytorch.kernels import LinearKernel, MaternKernel, RBFKernel, ScaleKernel
+from gpytorch.kernels.polynomial_kernel import PolynomialKernel
+from gpytorch.likelihoods import GaussianLikelihood, Likelihood
+from gpytorch.models import ExactGP
+from gpytorch.priors.torch_priors import GammaPrior
+from torch import Tensor
+from utils import SustainableConcreteDataset, get_bounds, get_day_zero_data
+
+
+class SustainableConcreteModel(object):
+    def __init__(
+        self,
+        strength_model: Optional[Model] = None,
+        gwp_model: Optional[Model] = None,
+        d: Optional[int] = None,
+    ):
+        self.strength_model = None
+        self.gwp_model = None
+        self.d = d
+
+    def fit_strength_model(self, data: SustainableConcreteDataset) -> None:
+        X, Y, Yvar, X_bounds = data.strength_data
+        self._set_d(X.shape[-1])
+        self.strength_model = fit_strength_gp(X, Y, Yvar, X_bounds)
+
+    def fit_gwp_model(self, data: SustainableConcreteDataset) -> None:
+        X, Y, Yvar, X_bounds = data.gwp_data
+        self._set_d(X.shape[-1] + 1)
+        self.gwp_model = fit_gwp_gp(X, Y, Yvar, X_bounds)
+
+    def _set_d(self, d: int) -> None:
+        if self.d is None:
+            self.d = d
+
+    def get_model_list(self) -> ModelListGP:
+        """Returns a ModelListGP modeling the GWP and compressive strength objectives as a function
+        of composition only.
+        Converts the strength and gwp models into a model list of independent models for gwp,
+        and x-day strengths, by fixing the time input of the strength model at 1 and 28 days.
+        """
+        models = [
+            self.gwp_model,
+            FixedFeatureModel(
+                base_model=self.strength_model, dim=self.d, indices=[self.d - 1], values=[1]
+            ),  # strength at day 1
+            FixedFeatureModel(
+                base_model=self.strength_model, dim=self.d, indices=[self.d - 1], values=[28]
+            ),  # strength at day 28
+        ]
+        model = ModelList(*models)
+        return model  # for use with multi-objective optimization
+
+    def plot_strength_curve(self, composition: Tensor, max_day: int = 28) -> None:
+        time = torch.arange(max_day + 1)
+        composition = composition.unsqueeze(0).expand(len(time))
+        # IDEA: use FixedFeatureModel?
+
+
+# BatchedMultiOutputGPyTorchModel, ExactGP
+class FixedFeatureModel(Model):
+    # advantage: only need to implement posterior for it to work with qNEHI
+    # disadvantage: makes the strength outputs independent
+    # TODO: check that these are appended before the InputTransforms are applied, not after.
+    def __init__(
+        self,
+        base_model: Model,
+        dim: int,
+        indices: Union[List[int], Tensor],
+        values: Union[List[float], Tensor],
+    ):
+        super().__init__()
+        self.base_model = base_model
+        if len(indices) != len(values):
+            raise ValueError("indices and values do not have the same length.")
+        indices = torch.as_tensor(indices)
+        values = torch.as_tensor(values)
+        self._indices = indices
+        self._fixed = torch.tensor([i in indices for i in torch.arange(dim, dtype=indices.dtype)])
+        self._values = values
+
+    def _add_fixed_features(self, X: Tensor):
+        """
+        Input:
+            - X: A (n x d)-dim Tensor.
+        Output:
+            A (n x (d + len(self._indices)))-dim Tensor.
+        """
+        Z = torch.zeros(*X.shape[:-1], X.shape[-1] + len(self._indices))
+        Z[..., self._fixed] = self._values
+        Z[..., ~self._fixed] = X
+        return Z
+
+    def forward(self, X: Tensor, *args, **kwargs):
+        return self.base_model.forward(self._add_fixed_features(X), *args, **kwargs)
+
+    def posterior(self, X: Tensor, *args, **kwargs):
+        return self.base_model.posterior(self._add_fixed_features(X), *args, **kwargs)
+
+
+def fit_gwp_gp(X: Tensor, Y: Tensor, Yvar: Tensor, X_bounds: Tensor):
+    """
+    Input:
+        X: Tensor of composition inputs without time (n x d).
+        Y: Tensor of GWP values (n x 1).
+        Yvar: Tensor of GWP variances (n x 1).
+
+    Output:
+        A FixedNoiseGP model fit to the data.
+    """
+    d_in = X.shape[-1]
+    d_out = Y.shape[-1]
+    if d_out != 1:
+        raise ValueError("Output dimensions is not one in gwp fitting.")
+    # GWP is a linear function of the inputs
+    kernel = LinearKernel()
+    model = FixedNoiseGP(
+        train_X=X,
+        train_Y=Y,
+        train_Yvar=Yvar,
+        covar_module=kernel,
+        input_transform=Normalize(d_in, bounds=X_bounds),
+        outcome_transform=Standardize(d_out),
+    )
+    mll = ExactMarginalLogLikelihood(model.likelihood, model)
+    fit_gpytorch_model(mll)
+    return model
+
+
+def fit_strength_gp(X: Tensor, Y: Tensor, Yvar: Tensor, X_bounds: Tensor):
+    """
+    Input:
+        X: Tensor of composition inputs including time (n x d).
+        Y: Tensor of strength values (n x 1).
+        Yvar: Tensor of strength variances (n x 1).
+
+    Output:
+        A FixedNoiseGP model fit to the data.
+    """
+    d_in = X.shape[-1]
+    d_out = Y.shape[-1]
+    if d_out != 1:
+        raise ValueError("Output dimensions is not one in strength curve fitting.")
+
+    # add data to condition GP to be zero at day zero
+    X_0, Y_0, Yvar_0 = get_day_zero_data(bounds=X_bounds, n=128)
+    X = torch.cat((X, X_0), dim=0)
+    Y = torch.cat((Y, Y_0), dim=0)
+    Yvar = torch.cat((Yvar, Yvar_0), dim=0)
+
+    # joint kernel to model all interactions
+    base_kernel = MaternKernel(
+        nu=2.5,
+        ard_num_dims=d_in,
+        lengthscale_constraint=None,
+        lengthscale_prior=None,
+    )
+    scaled_base_kernel = ScaleKernel(
+        base_kernel=base_kernel,
+        outputscale_prior=GammaPrior(2.0, 0.15),
+    )
+
+    # additive kernel to model behavior w.r.t. time
+    # try matern?
+    time_kernel = RBFKernel(  # MaternKernel # smoother RBF seems to work better for additive components
+        active_dims=torch.tensor([d_in - 1]),  # last dimension is time
+        ard_num_dims=1,
+        lengthscale_constraint=None,
+        # lengthscale_prior=None,
+    )
+    scaled_time_kernel = ScaleKernel(
+        base_kernel=time_kernel,
+        outputscale_prior=GammaPrior(2.0, 0.15),
+        # batch_shape=batch_shape,
+    )
+
+    # IDEA: + scaled_water_kernel and other additive components
+    kernel = scaled_base_kernel + scaled_time_kernel
+    model = FixedNoiseGP(
+        train_X=X,
+        train_Y=Y,
+        train_Yvar=Yvar,
+        covar_module=kernel,
+        input_transform=get_strength_gp_input_transform(X_bounds),
+        outcome_transform=Standardize(d_out),
+    )
+    mll = ExactMarginalLogLikelihood(model.likelihood, model)
+    fit_gpytorch_model(mll)
+    return model
+
+
+def get_strength_gp_input_transform(bounds: Tensor):
+    """Chains a log(time + 1) and Normalize transform on d dimensional input data,
+    with the provided bounds.
+    """
+    d = bounds.shape[-1]
+    time_index = [d - 1]
+    tf1 = AffineInputTransform(  # adds one to time dimension before taking log
+        d,
+        coefficient=torch.ones(1),
+        offset=torch.ones(1),
+        indices=time_index,
+        reverse=True,
+    )
+    tf2 = Log10(indices=time_index)  # taking log of time dimension for better extrapolation
+    transformed_bounds = tf2(tf1(bounds))
+    tf3 = Normalize(d, bounds=transformed_bounds)  # normalizing after log(t + 1) transform
+    return ChainedInputTransform(tf1=tf1, tf2=tf2, tf3=tf3)
---
+++ utils.py
@@ -0,0 +1,365 @@
+# utils module with data loaders and more.
+from typing import List, Tuple
+
+import pandas as pd
+import torch
+from torch import Tensor
+
+_TOTAL_BINDER_NAMES = ["Cement", "Fly Ash", "Slag"]
+_PASTE_CONTENT_NAMES = _TOTAL_BINDER_NAMES + ["Water"]
+_TOTAL_MASS_NAMES = _PASTE_CONTENT_NAMES + ["HRWR", "Coarse Aggregate", "Fine Aggregate"]
+_VERBOSE = False
+
+
+class SustainableConcreteDataset(object):
+    def __init__(
+        self,
+        X: Tensor,
+        Y: Tensor,
+        Ystd: Tensor,
+        X_columns: List[str],
+        Y_columns: List[str],
+        Ystd_columns: List[str],
+    ):
+        if X_columns[-1] != "Time":
+            raise ValueError(f"Last dimension of X assumed to be time, but is {X_columns[-1]}.")
+
+        # making sure we are not overwriting these
+        self._X_columns = X_columns
+        self._Y_columns = Y_columns
+        self._Ystd_columns = Ystd_columns
+        self._X = X
+        self._Y = Y
+        self._Ystd = Ystd
+
+    @property
+    def X(self):
+        return self._X
+
+    @property
+    def Y(self):
+        return self._Y
+
+    @property
+    def Ystd(self):
+        return self._Ystd
+
+    @property
+    def Yvar(self):
+        return self.Ystd.square()
+
+    @property
+    def strength_data(self):
+        X_bounds = get_bounds(self.X_columns)
+        return self.X, self.Y[:, [1]], self.Yvar[:, [1]], X_bounds
+
+    @property
+    def unique_compositions(self):
+        c = self.X[:, :-1]
+        c_unique, rev = c.unique(dim=0, sorted=False, return_inverse=True)
+        return c_unique, rev
+
+    @property
+    def unique_composition_indices(self):
+        c, rev = self.unique_compositions
+        rev = [r.item() for r in rev]  # converting to a list of python ints
+        # indices of first occurances of unique compositions
+        unique_indices = [rev.index(i) for i in range(len(c))]
+        # sorting in ascending order, to be identical to collection order
+        unique_indices.sort()
+        return unique_indices
+
+    @property
+    def gwp_data(self):
+        # removes duplicates due to multiple measurements in time, which is irrelevant for gwp
+        unique_indices = self.unique_composition_indices
+        X = self.X[unique_indices, :-1]
+        Y = self.Y[unique_indices, 0].unsqueeze(-1)
+        Yvar = self.Yvar[unique_indices, 0].unsqueeze(-1)
+        X_bounds = get_bounds(self.X_columns[:-1])  # without time dimension
+        return X, Y, Yvar, X_bounds
+
+    @property
+    def X_columns(self):
+        return self._X_columns
+
+    @property
+    def Y_columns(self):
+        return self._Y_columns
+
+    @property
+    def Ystd_columns(self):
+        return self._Ystd_columns
+
+
+def load_concrete_strength(
+    data_path: str = "data/concrete_strength.csv", verbose: bool = _VERBOSE, dtype=torch.float32
+):
+    # loading csv into dataframe
+    df = pd.read_csv(data_path, delimiter=",")
+
+    if verbose:
+        print(f"The data has {len(df)} rows and {len(df.columns)} columns, which are:")
+        for column in df.columns.to_list():
+            print("\t-", column)
+        print()
+
+    data_index = 3
+    data_columns = df.columns[data_index:]
+    n_missing = torch.tensor(df[data_columns].to_numpy()).isnan().sum(dim=0)
+    missing_ind = n_missing > 0
+    if verbose and missing_ind.any():
+        print(f"There are {missing_ind.sum()} columns with missing entries:")
+        for name, missing in zip(data_columns[missing_ind], n_missing[missing_ind]):
+            print("\t-", name, "has", missing.item(), "missing entries.")
+        print()
+
+    # throw out one row with missing strength std
+    ind = df.isna().any(axis=1)
+    df = df.drop(df[ind].index)
+    if verbose:
+        print(
+            "Removed rows with missing entries."
+            f"The data now has {len(df)} rows and {len(df.columns)} columns."
+        )
+        print()
+
+    if verbose:
+        print("Removing Dec_2022_6 data since it was cured at a different temperature.")
+        print()
+    df = df.drop(df[df["Mix ID"] == "Dec_2022_6"].index, axis=0)
+
+    # separating columns as inputs, outputs, and output uncertainties
+    X_columns = df.columns[3:-6].to_list()
+    Y_columns = df.columns[[-6, -4]].to_list()
+    Ystd_columns = df.columns[[-5, -3]].to_list()
+    if verbose:
+        print("Separating model inputs and outputs:")
+        print(f"Input columns: ")
+        for col in X_columns:
+            print("\t-", col)
+        print(f"Output (Mean) columns")
+        for col in Y_columns:
+            print("\t-", col)
+        print(f"Output (Std) columns")
+        for col in Ystd_columns:
+            print("\t-", col)
+        print()
+
+    # casting dataframe to torch tensors
+    X = torch.tensor(df[X_columns].to_numpy(), dtype=dtype)
+    Y = torch.tensor(df[Y_columns].to_numpy(), dtype=dtype)
+    Ystd = torch.tensor(df[Ystd_columns].to_numpy(), dtype=dtype)
+
+    if verbose:
+        print(f"Negating GWP to frame as joint maximization problem.")
+        print()
+    Y[:, 0] = -Y[:, 0]
+
+    if verbose:
+        print(
+            "Setting standard deviation of GWP to uniformly small value "
+            "since our estimates are deterministic."
+        )
+        print()
+    Ystd[:, 0] = 1e-3  # allowing us to use the same model type (FixedNoiseGP)
+
+    # dividing empirical standard deviations of strength by the number of measurements.
+    if verbose:
+        print(
+            "Computing strength standard error of by "
+            f"dividing standard deviation by sqrt({df.columns[-2]})."
+        )
+        print()
+    n_measurements = torch.tensor(df[df.columns[-2]].to_numpy(), dtype=dtype)
+    Ystd[:, 1] /= n_measurements.sqrt()
+
+    return SustainableConcreteDataset(
+        X=X, Y=Y, Ystd=Ystd, X_columns=X_columns, Y_columns=Y_columns, Ystd_columns=Ystd_columns
+    )
+
+
+def get_bounds(X_columns, verbose: bool = _VERBOSE) -> Tensor:
+    """Returns bounds of columns in X, ignoring time, assumed to be the last dimension."""
+    # if X_columns[-1] != "Time":
+    #     raise ValueError(f"Last dimension of X assumed to be time, but is {X_columns[-1]}.")
+    bounds_dict = {
+        # NOTE: the pure cement baseline is outside of these bounds (~752), as is Dec_2022_2 (~211)
+        "Cement": (300, 700),
+        "Fly Ash": (0, 350),
+        "Slag": (0, 450),
+        "Coarse Aggregate": (800, 1950),
+        "Fine Aggregate": (600, 1700),
+        "Time": (0, 28),  # up to 28 days
+    }
+
+    min_binder, max_binder = 0, 0
+    for name in _BINDER_NAMES:
+        min_binder += bounds_dict[name][0]
+        max_binder += bounds_dict[name][1]
+
+    bounds_dict.update(
+        {
+            "Water": (0.2 * min_binder, 0.5 * max_binder),
+            "HRWR": (0, 0.1 * max_binder),  # linear constraint also applies, see below
+        }
+    )
+    bounds = torch.tensor([bounds_dict[col] for col in X_columns]).T
+    if verbose:
+        print("The lower and upper bounds for the respective variables are set to:")
+        for col, bound in zip(X_columns, bounds.T):
+            print(f"\t- {col}: [{bound[0].item()}, {bound[1].item()}]")
+        print()
+    return bounds
+
+
+def get_constraints(X_columns, verbose: bool = _VERBOSE):
+    # inequality constraints
+    inequality_dict = {
+        "Total Binder": (510, 1000),
+        "Total Mass": (3600, 4400),
+        "Paste Content": (0.16, 0.35),  # as a proportion of total mass
+        "Water": (0.2, 0.5),  # as a proportion of total binder
+        "HRWR": (0, 0.1),  # as a proportion of total binder
+    }
+    if verbose:
+        print("Adding linear constraints with lower and upper limits:")
+        for key in inequality_dict:
+            print("\t-", key, ":", inequality_dict[key])
+        print(
+            "NOTE: the paste content constraint is proportional to the total mass, "
+            "and the water and HRWR constraints are proportional to the total binder."
+        )
+        print()
+
+    constraints = [
+        *get_mass_constraints(X_columns, *inequality_dict["Total Mass"]),
+        *get_binder_constraints(X_columns, *inequality_dict["Total Binder"]),
+        *get_paste_constraints(X_columns, *inequality_dict["Paste Content"]),
+        *get_water_constraints(X_columns, *inequality_dict["Water"]),
+        *get_hrwr_constraints(X_columns, *inequality_dict["HRWR"]),
+    ]
+    return constraints
+
+
+def get_mass_constraints(X_columns: List[str], lower: float, upper: float) -> List:
+    return get_sum_constraints(
+        X_columns=X_columns, subset_names=_TOTAL_MASS_NAMES, lower=lower, upper=upper
+    )
+
+
+def get_binder_constraints(X_columns: List[str], lower: float, upper: float) -> List:
+    return get_sum_constraints(
+        X_columns=X_columns, subset_names=_TOTAL_BINDER_NAMES, lower=lower, upper=upper
+    )
+
+
+def get_paste_constraints(X_columns: List[str], lower: float, upper: float):
+    # Paste content = (Cement + Slag + Fly Ash + Water)
+    # Constraint: lower < (Paste content) / (Total Mass) < upper
+    # i.e. a proportional sum constraint
+    return get_proportional_sum_constraints(
+        X_columns=X_columns,
+        numerator_names=_PASTE_CONTENT_NAMES,
+        denominator_names=_TOTAL_MASS_NAMES,
+        lower=lower,
+        upper=upper,
+    )
+
+
+def get_water_constraints(X_columns: List[str], lower: float, upper: float):
+    # Constraint: lower < (Water) / (Total Binder) < upper
+    # i.e. a proportional sum constraint
+    return get_proportional_sum_constraints(
+        X_columns=X_columns,
+        numerator_names=["Water"],
+        denominator_names=_TOTAL_BINDER_NAMES,
+        lower=lower,
+        upper=upper,
+    )
+
+
+def get_hrwr_constraints(X_columns: List[str], lower: float, upper: float):
+    # Constraint: lower < (HRWR) / (Total Binder) < upper
+    # i.e. a proportional sum constraint
+    return get_proportional_sum_constraints(
+        X_columns=X_columns,
+        numerator_names=["HRWR"],
+        denominator_names=_TOTAL_BINDER_NAMES,
+        lower=lower,
+        upper=upper,
+    )
+
+
+def get_sum_constraints(
+    X_columns: List[str], subset_names: List[str], lower: float, upper: float
+) -> List:
+    _, coeffs = get_subset_sum_tensors(X_columns=X_columns, subset_names=subset_names)
+    # can throw out indices for which coeffs is zero if we don't recombine coefficients
+    nz_ind = coeffs != 0
+    ind, coeffs = torch.arange(len(coeffs))[nz_ind], coeffs[nz_ind]
+    return [(ind, coeffs, lower), (ind, -coeffs, -upper)]
+
+
+def get_proportional_sum_constraints(
+    X_columns: List[str],
+    numerator_names: List[str],
+    denominator_names: List[str],
+    lower: float,
+    upper: float,
+):
+    """Converts a constraint on a fraction of two subset sums into a linear form, i.e. if the constraint is of the form
+
+        lower < (sum of numerator_names) / (sum of denominator_names) < upper,
+
+    then (numerator) < upper * (denominator) and so
+
+        upper * (denominator) - (numerator) > 0, and
+        (numerator) - lower * (denominator) > 0.
+    """
+    _, num_coeffs = get_subset_sum_tensors(X_columns=X_columns, subset_names=numerator_names)
+    _, den_coeffs = get_subset_sum_tensors(X_columns=X_columns, subset_names=denominator_names)
+
+    # upper constraint
+    upper_coeffs = upper * den_coeffs - num_coeffs
+    upper_nz_ind = upper_coeffs != 0
+    upper_ind = torch.arange(len(upper_coeffs))[upper_nz_ind]
+    upper_coeffs = upper_coeffs[upper_nz_ind]
+
+    # lower constraint
+    lower_coeffs = num_coeffs - lower * den_coeffs
+    lower_nz_ind = lower_coeffs != 0
+    lower_ind = torch.arange(len(lower_coeffs))[lower_nz_ind]
+    lower_coeffs = lower_coeffs[lower_nz_ind]
+
+    return [(upper_ind, upper_coeffs, 0.0), (lower_ind, lower_coeffs, 0.0)]
+
+
+def get_subset_sum_tensors(X_columns: List[str], subset_names: List[str]) -> Tuple[Tensor, Tensor]:
+    indices = [X_columns.index(name) for name in subset_names]
+    coeffs = torch.zeros(len(X_columns))
+    coeffs[indices] = 1
+    return indices, coeffs
+
+
+def get_reference_point():
+    gwp = -430.0  # based on existing minimum in the data (pure cement)
+    strength_day_1 = 1000
+    # strength_day_7 = 3000
+    strength_day_28 = 5000
+    return torch.tensor([gwp, strength_day_1, strength_day_28])
+
+
+def get_day_zero_data(bounds: Tensor, n: int = 128):
+    """Computes a tensor n sobol points that satisfy the bounds, appended with a
+    zeros tensor. Useful to condition the strength GP to be zero at day zero.
+    """
+    d = bounds.shape[-1]
+    sobol_engine = torch.quasirandom.SobolEngine(dimension=(d - 1))  # excluding time
+    X_0 = sobol_engine.draw(n)
+    X_0 = torch.cat((X_0, torch.zeros(n, 1)), dim=-1)  # append time (zero)
+    a, b = bounds[0], bounds[1]
+    X_0 = (b - a) * X_0 + a  # scaling according to bounds
+    Y_0 = torch.zeros(n, 1)  #  zero strength
+    Yvar_0 = torch.full((n, 1), 1e-4)  #  with large certainty
+    return X_0, Y_0, Yvar_0
